b96994cab3d934bc01d8d64ff6b8ed884c541680
Mita-21 <75340818+Mita-21@users.noreply.github.com>
2022-03-24 18:19:37 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+//testeo app
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/React Context API/i);
